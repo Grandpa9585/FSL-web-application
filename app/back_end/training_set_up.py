3dbd6108ff9cb719967c30ac0398648e9b835449
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-03-06 14:57:38 +0800

    trainging setup

--- app/back_end/training_set_up.py
+++ app/back_end/training_set_up.py
@@ -4,6 +4,46 @@ import numpy as np
 
 import functions
 
+import os
+import splitter
+
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.utils import to_categorical
+
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
+from tensorflow.keras.callbacks import TensorBoard
+
+label_map = {label:num for num, label, in enumerate(splitter.actions)}
+
+sequences, labels =[], []
+for action in splitter.actions:
+    for sequence in range(77):
+        window = []
+        for frame_num in range(109):
+            res = np.load(os.path.join(splitter.flepath, action, str(sequence + 1), "{}.npy".format(frame_num)))
+            window.append(res)
+        sequences.append(window)
+        labels.append(label_map[action])
+
+X = np.array(sequences)
+
+y = to_categorical(labels).astype(int)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.5)
+
+log_dir = os.path.join('Logs')
+tb_callback = TensorBoard(log_dir=log_dir)
+
+model = Sequential()
+model.add(LSTM(64, return_sequences=True, activation='relu', input_shape=(109,1662)))
+model.add(LSTM(128, return_sequences=True, activation='relu'))
+model.add(LSTM(64, return_sequences=False, activation='relu'))
+model.add(Dense(64, activation='relu'))
+model.add(Dense(32, activation='relu'))
+model.add(Dense(splitter.actions.shape[0], activation='softmax'))
+
+model.compile(optimi)
 # number of videos per action
 no_sequences = 7
 # number of frames per video 
